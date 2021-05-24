cc0b7c4784557d93bb799afaffacd00c896ed62f
1e1001 <michael@malinov.com>
2021-05-23 20:14:31 -0700

    paine

---
+++ assets/scripts/mail.js
@@ -0,0 +1,9 @@
+(function(){
+  var e=document.getElementById("mail"),l,t="mailto:",i=1;
+  if(e!==undefined){
+    l=e.childNodes;
+    for(;i<l.length-1;i++)t+=l[i].data?l[i].data:l[i].alt;
+    e.onclick=e.onkeydown=function(e){var k=e.keyCode||e.which;if(!k||k===13)window.open(t)};
+    e.className="flink"
+  }
+})();
