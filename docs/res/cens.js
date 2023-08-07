461ab1f53cb31b859c3e29fda953fd4400827ebc
1e1001 <michael@malinov.com>
2023-08-07 03:07:30 -0700

    fix cens (real)

--- docs/res/cens.js
+++ docs/res/cens.js
@@ -1,24 +1,13 @@
-if(!window.matchMedia(`(prefers-reduced-motion:reduce)`).matches){
+(function(){
+if(!window.matchMedia("(prefers-reduced-motion:reduce)").matches){
 var table='0123456789¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğ !"#$%&\'()*+,-./:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ͰͱͲͳʹ͵Ͷͷͺͻͼͽ;Ϳ΄΅Ά·ΈΉΊΌΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώϏϐϑϒϓϔϕϖϗϘϙϚϛϜϝϞϟϠϡϰϱϲϳϴϵ϶ϷϸϹϺϻϼϽϾϿЀЁЂЃЄЅІЇЈЉЊЋЌЍЎЏАБВГДЕЖЗИЙКЛМНОПРСТΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡԣԤԥԦԧԨԩԮԯءؤةرزهوڑڒړڔږڗژڙ՟गदपबमवषहէշაბევზიკმნპჟსუქყშჩძწჭხჰჱჲჳჴჸჺ჻ᐜᑉכװۆۇۈۋە܅܆܇ܕܖܝܪܯᴀᴁᴂᴃᴄᴅᴆᴇᴈᴉᴊᴋᴌᴍᴎᴏᴐᴑᴒᴓᴔᴕᴖᴗᴘᴙᴚᴛᴜᴝᴞᴟᴠᴡᴢᴣᴤᴥᴦᴧᴨᴩᴪᴫᴬᴭᴮᴯᴰᴱᴲᴳᴴᴵᴶᴷᴸᴹᴺᴻᴼᴽᴾᴿᵀᵁᵂᵃᵄᵅᵆᵇᵈᵉᵊᵋᵌᵍᵎᵏᵐᵑᵒᵓᵔᵕᵖᵗᵘᵙᵚᵛᵜᵝᵞᵟᵠᵡᵢᵣᵤᵥᵦᵧᵨᵩᵪᵫᵬᵭᵮᵯᵰᵱᵲᵳᵴᵵᵶᵷᵸᵹᵺᵻᵼᵽᵾᵿݥݦݿށބވމފތލގޒޔޗޛޡޢޥḀḁḂḃḄḅḆḇḈḉḊḋḌḍḎḏḐḑḒḓḔḕḖḗḘḙḚḛḜḝḞḟḠḡḢḣḤḥḦḧḨḩḪḫḬḭḮḯḰḱḲḳḴḵḶḷḸḹḺḻḼḽḾḿṀṁṂṃṄṅṆṇṈṉṊṋṌṍṎṏṐṑṒṓṔṕṖṗṘṙṚṛṜṝṞṟṠṡṢṣṤṥṦṧṨṩṪṫṬṭṮṯṰṱṲṳṴṵṶṷṸṹṺߙߞߠߣߤߥ℀℁ℂ℃℅℆ℇ℈℉ℍℎℏℓℕ№℗ℙℚℝ℠℡™ℤΩ℧℩KÅ℮Ⅎℴℵℶℹ℻ℼℽℾℿ⅁⅂⅃⅄ⅅⅆⅇⅈⅉ⅋⅍ⅎ⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞ⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩⅪⅫⅬⅭⅮⅯⅰⅱⅲⅳⅴⅵⅶⅷⅸⅹⅺⅻⅼⅽⅾⅿ࠻∀∁∂∃∄∅∆∇∈∉∊∋∌∍∎ࡂࡘग़ॿইঌগঙছথপলশসৡ০২৩৪৫৬৭৮৯৵ⱠⱢⱣⱤⱥⱦⱧⱨⱩⱪⱫⱬⱭⱮⱯⱰⱱⱲⱳⱴⱵⱶⱷⱹⱻⱼⱽⱾⱿⲁⲏⲑⲗⲛⲟⲡⲢⲩⲯⲳⲹⲻⳅⳊⳍⳐⳗⳟ੬ઢરઽ૧୧୨୮୯உப௦క౺ขจชถยฤลวสอฮ฿แ๐๑๖ງໄ༈༡༢༣༦༨༩࿅࿈ሃሄበባቦቨቫቮቯኒኔዕዘዛዞዩጎጘጞፅ፥፦᎑᎔ᎨᎮᎰᎼᏐᏓᏗ ᚁᚆᚋᚐᚕ᚛᚜ᛱᛶᛷᜠᜡᜢᜣᜤᜥᜦᜧᜨᜩᜪᜫᜬᜭᜮᜰᜱ៖ᠵᠶᡏᡛᡪᡲᡷᢃᢄᢌᢏᢞᢣᢦᢾᢿᣃᣄᣔᣛᤚ᥅᥇᥈᥊᥋᥌᥍᥎᥏ᥠᥤᥥᥦᥩᥪᥫᥳ᧒᧙᨟ᩡ᪒᭐᭔᭢᭤᯿ᰚᰛᰬᰭᰮᰯᰰᰱᰲᰳᰶ᰷᰽᱀᱁᱇᱉᱑᱒᱓᱔᱖᱗ᱻᳱᳲᳵᶀᶁᶂᶃᶄᶅᶆᶇᶈᶉᶊᶋᶌᶍᶎᶏᶐᶑᶒᶓᶔᶕᶖᶗᶘᶙᶚᶛᶜᶝᶞᶟᶠᶡᶢᶣᶤᶥᶦᶧᶨᶩᶪᶫᶬᶭᶮᶯᶰᶱᶲᶳᶴᶵᶶᶷᶸᶹᶺᶻᶼᶽᶾᶿỻỼỽỾỿἀἁἂἃἄἅἆἇἈἉἊἋἌἍἎἏἐἑἒἓἔἕἘἙἚἛἜἝἠἡἢἣἤἥἦἧἨἩἪἫἬἭἮἯἰἱἲἳἴἵἶἷἸἹἺἻἼἽἾἿὀὁὂὃὄὅὈὉὊὋὌὍὐὑὒὓὔὕὖὗὙὛὝὟὠὡὢὣὤὥὦὧὨὩὪὫὬὭὮὯὰάὲέὴήὶίὸόὺύὼώᾀᾁᾂᾃᾄᾅᾆᾇᾈᾉᾊᾋᾌᾍᾎᾏᾐᾑᾒᾓᾔᾕᾖᾗᾘᾙᾚᾛᾜᾝᾞᾟᾠᾡᾢᾣᾤᾥᾦᾧᾨᾩᾪᾫᾬᾭᾮᾯᾰᾱᾲᾳᾴᾶᾷᾸᾹᾺΆᾼ᾽ι᾿῀῁ῂῃῄῆῇῈΈῊΉῌ῍῎῏ῐῑῒΐῖῗῘῙῚΊ῝῞῟ῠῡῢΰῤῥῦῧῨῩῪΎῬ῭΅`ῲῳῴῶῷῸΌῺΏῼ´῾‐‑‒–‖‗‘’‚‛“”„‟†‡•‣‧′″‴‵‶‷‸‹›‼‽‾‿⁀⁁⁃⁄⁅⁆⁇⁈⁉⁊⁋⁌⁍⁎⁏⁐⁑⁒⁔⁕⁖⁗⁘⁙⁚⁛⁜⁝⁞⁰ⁱ⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾ⁿ₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎ₐₑₒₓₔₕₖₗₘₙₚₛₜ₣₤₥₦₧₨₩₫€₭₮₯₴₸₹₽ꀈꀞꀦꀧꀨꀭꀮꀯꁊꁦꁧꁳꁴꁵꁼↀↁↂↃↄↅↆↇↈ↉↊↋⎕⎛⎜⎝⎞⎟⎠⎡⎢⎣⎤⎥⎦⎧⎨⎩⎪⎫⎬⎭⎮⎰⎱␀␁␂␃␄␅␆␇␈␉␊␋␌␍␎␏␐␑␒␓␔␕␖␗␘␙␚␛␜␝␞␟␠␡␢␣␤␦ꓙꓩꔷꙁꙃꙄꙅꙋꙌꙍꙖꙗꚃꚊꚋꚍꚑꚘꚙꚜꚝꚳꚹⰓ꜀꜁꜂꜃꜄꜅꜆꜇꜈꜉꜊꜋꜌꜍꜎꜏꜐꜑꜒꜓꜔꜕꜖ꜛꜜꜝꜞꜟꜢꜣꜤꜥꜦꜧꜰꜱꜲꜳꜴꜵꜾꜿꝀꝁꝈꝊꝋꝎꝏꝚꝛꝟꝣꝤꝥꝦꝧꝩꝫꝬꝭꝵꝸꝻꝽꝾꝿⰰⰳⰵⰷⱈⱍⱏⱔⱖⱛ⳥⳦ⴭⴾⵆⵊⵯ⸀⸁⸂⸃⸄⸅⸆⸇⸈⸉⸊⸋⸌⸍⸘⸜⸝⸞⸟⸢⸣⸤⸥⸨⸩⸮ⸯ⸰⸱⸲⸳⸴⸵⸸⸹⹁⹂꒔꒗꒘꒞꒠꒣꒩꒯꒸꒹꒻꒼꒾꓃꓄ꗚ꘍꘎꘦꘨ꛌꛒꛕꛚꛦꛧꛨꛩꛪꛬ꛵ꞀꞁꞄꞇꞈ꞉ꞋꞌꞍꞎꞏꞐꞑꞒꞓꞔꞕꞘꞙꞩꞪꞫꞬꞭꞮꞰꞱꞲꞳꞴꞵꞶꞷꟸꟹꟺꟻꟼꠁꠑ꠷ꡊꡘꡦꡫ꣓꣔꣖꣙ꣲꣳꣴꣵꣶꣷꣻ꤮ꤰꤳꤴꤹ꧈ꪅꪶꪻꪼꬰꬵꬿꭉꭓꭗꭙ꭛ꭜꭞꭠꭡꭢꭣꭤꭥꭶꭻꭾꮀꮁꮓꮛꮞꮣꮪꮮꮷꮽꮾ꯫ﬁﬂכּכֿﭬﭭﭰﭱﮃﮅﮇﮉﮊﮌﮐﮔﮘﮜﮬﳙﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ�ﯕﯗﯘﯙﯚﯛﯜﯞﯟﯪﺀﺅﺆﺓﺪﺬﺭﺯﻌﻏﻐﻓﻔﻗﻘﻛﻩﻫﻭﻮ｡｢｣､･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀ'
-var ctx=document.createElement("canvas").getContext("2d")
+var ctx=document.createElement("canvas").getContext("2d"),lst=Array.from(document.getElementsByClassName("cens"))
 ctx.font="400 16px Iosevka Fuck Web"
+function frame(){requestAnimationFrame(frame)
+for(var i=0;i<lst.length;++i){var ch=new Array(lst[i][1])
+for(var j=0;j<ch.length;++j)ch[j]=table[Math.floor(Math.random()*table.length)]
+lst[i][0].setAttribute("data-c",ch.join(""))}}
 function text_size(text){return ctx.measureText(text).width}
-var ch_width=0,lst=Array.from(document.getElementsByClassName("cens"))
-function frame1(){
-ch_width=text_size("_");
-if(ch_width>0){
-for(var i=0;i<lst.length;++i){
-var o=lst[i]
-lst[i]=[o,Math.floor(text_size(o.innerText)/ch_width+.1)]
-o.className="cens-js"
-o.innerHTML="<span>"+o.innerHTML+"</span>"}
-requestAnimationFrame(frame2)}else
-requestAnimationFrame(frame1)}
-function frame2(){
-requestAnimationFrame(frame2)
-for(var i=0;i<lst.length;++i){
-var ch=new Array(lst[i][1])
-for(var j=0;j<ch.length;++j)
-ch[j]=table[Math.floor(Math.random()*table.length)]
-lst[i][0].setAttribute("data-c",ch.join(""))
-}}frame1()}
+document.fonts.ready.then(function(){var ch_width=text_size("_")
+if(ch_width>0){for(var i=0,o;i<lst.length;++i)o=lst[i],lst[i]=[o,Math.floor(text_size(o.innerText)/ch_width+.1)],o.className="cens-js",o.innerHTML="<span>"+o.innerHTML+"</span>"
+requestAnimationFrame(frame)}})}})()
